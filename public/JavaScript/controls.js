ea1ff718e18cf1279b4cce3e619d25d8b8cb7251
LTapsy <drolxiv@gmail.com>
2019-12-12 14:11:10 -0700

    game done

--- public/JavaScript/controls.js
+++ public/JavaScript/controls.js
@@ -115,7 +115,24 @@ function moveLeft(){
 
 }
 function moveRight(){
-    frontEndBoard.style = "animation-play-state: running;";
+    wanted.style = "animation-play-state: running;"
+    aboutBoard.style = "animation-play-state: running;"
+    frontEndBoard.style = "animation-play-state: running;"
+    crate2.style = "animation-play-state: running;"
+    skills.style = "animation-play-state: running;"
+    skills1.style = "animation-play-state: running;"
+    skills2.style = "animation-play-state: running;"
+    educationBoard.style = "animation-play-state: running;"
+    education.style = "animation-play-state: running;"
+    crate1.style = "animation-play-state: running;"
+    workBoard.style = "animation-play-state: running;"
+    mcd.style = "animation-play-state: running;"
+    lubecity.style = "animation-play-state: running;"
+    papajohns.style = "animation-play-state: running;"
+    chicken.style = "animation-play-state: running;"
+    oil.style = "animation-play-state: running;"
+    decalCityBoard.style = "animation-play-state: running;"
+
     ground.style = "animation-play-state: running;";
     frontPlants.style = "animation-play-state: running;";
     frontTrees.style = "animation-play-state: running;";
@@ -124,7 +141,25 @@ function moveRight(){
 }
 
 function movementStop(){
-    frontEndBoard.style = "animation-play-state: paused;";
+    wanted.style = "animation-play-state: paused;"
+    aboutBoard.style = "animation-play-state: paused;"
+    frontEndBoard.style = "animation-play-state: paused;"
+    crate2.style = "animation-play-state: paused;"
+    skills.style = "animation-play-state: paused;"
+    skills1.style = "animation-play-state: paused;"
+    skills2.style = "animation-play-state: paused;"
+    educationBoard.style = "animation-play-state: paused;"
+    education.style = "animation-play-state: paused;"
+    crate1.style = "animation-play-state: paused;"
+    workBoard.style = "animation-play-state: paused;"
+    mcd.style = "animation-play-state: paused;"
+    lubecity.style = "animation-play-state: paused;"
+    papajohns.style = "animation-play-state: paused;"
+    chicken.style = "animation-play-state: paused;"
+    oil.style = "animation-play-state: paused;"
+    decalCityBoard.style = "animation-play-state: paused;"
+
+
     ground.style = "animation-play-state: paused;";
     frontPlants.style = "animation-play-state: paused;";
     frontTrees.style = "animation-play-state: paused;";
